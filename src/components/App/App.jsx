0a171b2142b907544373559c60553c6c281ba22c
Kateryna-4yka <kotasyri@gmail.com>
2025-02-24 15:00:24 +0200

    3

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,16 @@
-import './App.css'
-import Section from "../Section/Section"
-
+import css from "./App.module.css";
+import Section from "../Section/Section";
+import "modern-normalize";
 
 
 
 export default function App() {
 
   return (
-    <>
+    <div className={css.div}>
 <Section />
 
-    </>
+    </div>
   )
 }
 
